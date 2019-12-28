8748ca40dda69bbc44a8a471529a96a65c1faebe
Anthony Liu <anthonyliuhao@icloud.com>
2019-12-27 23:16:45 -0800

    Use for loop and timer to display characters one by one

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,24 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = ""
+        let titleText = "⚡️FlashChat"
+        
+        // You can't subscript a string, so work around it by converting it to an array
+        let titleCharArray = Array(titleText)
+        
+        // As each timer starts at the same time, subsequent timers need to wait longer to fire off
+        for index in 0..<titleCharArray.count {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(index), repeats: false) { (timer) in
+                self.titleLabel.text?.append(titleCharArray[index])
+            }
+        }
     }
     
-
+    
 }
