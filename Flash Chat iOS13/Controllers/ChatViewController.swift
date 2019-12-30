46759b57ef23c65f2f59702d273f85bfa83eb537
Anthony Liu <anthonyliuhao@icloud.com>
2019-12-29 17:13:02 -0800

    Save sent message data to Firebase

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -13,6 +13,9 @@ class ChatViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    let db = Firestore.firestore()
+    
     var messages: [Message] = [
         Message(sender: "one@two.com", body: "Hey!"),
         Message(sender: "three@four.com", body: "Hello!"),
@@ -38,8 +41,22 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody
+            ]) { (error) in
+                if let e = error {
+                    print("There was an issue saving data to Firestore: \(e.localizedDescription)")
+                }
+                else {
+                    print("Successfully save data!")
+                }
+            }
+        }
     }
-    
+        
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
         let firebaseAuth = Auth.auth()
         
